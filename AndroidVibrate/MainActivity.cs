71be214cd87f33e4b2b73530d8e54b536d1f20dc
vrutti.lad <vrutti.lad@1R-39>
2022-02-10 17:23:08 +0530

    Add project files.

---
+++ AndroidVibrate/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
+
+namespace AndroidVibrate
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+       
+
+        private Button button;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+            
+        }
+
+     
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Use default vibration length
+                Vibration.Vibrate();
+
+                // Or use specified time
+                var duration = TimeSpan.FromSeconds(3);
+                Vibration.Vibrate(duration);
+                Toast.MakeText(this, "VIBRATION ", ToastLength.Short).Show();
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Log.Debug("hjhhg", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
